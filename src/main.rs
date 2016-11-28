2ea79908036eb2829eba723f8c440281626ae920
Victor Polevoy <maintainer@vpolevoy.com>
2016-11-28 15:51:39 +0300

    New output format, documentation link, LICENSE file

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::env;
 
 fn main() {
     let us = UrlShortener::new(); 
-    for (i, link) in env::args().skip(1).enumerate() {
-        if let Ok(short) = us.try_generate(link) {
-            println!("{}: {}", i + 1, short);
+    for link in env::args().skip(1) {
+        if let Ok(short) = us.try_generate(link.clone()) {
+            println!("{} -> {}", link, short);
         } else {
-            println!("{}: Error", i);
+            println!("{} -> Error", link);
         }
     }
 }
