8faddb3405690249560c38e5d27673f2e42317b7
Victor Polevoy <fx@thefx.co>
2018-01-06 19:53:27 +0300

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use urlshortener::UrlShortener;
 use std::env;
 
 fn main() {
-    let us = UrlShortener::new(); 
+    let us = UrlShortener::new().unwrap();
     for link in env::args().skip(1) {
-        if let Ok(short) = us.try_generate(link.clone()) {
+        if let Ok(short) = us.try_generate(&link, None) {
             println!("{} -> {}", link, short);
         } else {
             println!("{} -> Error", link);
