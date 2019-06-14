7464603fc0ee9f0990a888d8724eb747afcaf90a
Victor Polevoy <fx@thefx.co>
2019-06-14 22:26:55 +0200

    Update to urlshortener 1

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-extern crate urlshortener;
-
-use std::env;
-use urlshortener::UrlShortener;
-
 fn main() {
+    use std::env;
+    use urlshortener::client::UrlShortener;
+
     let us = UrlShortener::new().unwrap();
     for link in env::args().skip(1) {
-        if let Ok(short) = us.try_generate(&link, None) {
-            println!("{} -> {}", link, short);
-        } else {
-            println!("{} -> Error", link);
+        let mut short_link = Err(urlshortener::providers::ProviderError::Connection);
+        for provider in urlshortener::providers::PROVIDERS {
+            let res = us.generate(&link, provider);
+            if res.is_ok() {
+                short_link = res;
+                break;
+            }
+        }
+        match short_link {
+            Ok(short) => println!("{} -> {}", link, short),
+            Err(e) => println!("{} -> {:?}", link, e),
         }
     }
 }
