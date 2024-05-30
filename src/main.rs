77f7e53e99c32f7ee89fa4ab458262c1e1db5140
Victor Polevoy <fx@thefx.co>
2024-05-30 14:26:49 +0200

    Support setting the clipboard content.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+const USE_CLIPBOARD_ARGUMENT_NAME: &str = "--use-clipboard";
+
 fn main() {
     use std::env;
     use urlshortener::client::UrlShortener;
 
+    let use_clipboard = env::args()
+        .skip(1)
+        .any(|arg| arg == USE_CLIPBOARD_ARGUMENT_NAME);
+
+    let mut shortened_links = Vec::new();
+
     let us = UrlShortener::new().unwrap();
     for link in env::args().skip(1) {
+        if link == USE_CLIPBOARD_ARGUMENT_NAME {
+            // Ignore the argument if it is not a link.
+            continue;
+        }
+
         let mut short_link = Err(urlshortener::providers::ProviderError::Connection);
         for provider in urlshortener::providers::PROVIDERS {
             let res = us.generate(&link, provider);
@@ -13,8 +26,18 @@ fn main() {
             }
         }
         match short_link {
-            Ok(short) => println!("{} -> {}", link, short),
+            Ok(short) => {
+                if use_clipboard {
+                    shortened_links.push(short.clone());
+                }
+
+                println!("{} -> {}", link, short)
+            }
             Err(e) => println!("{} -> {:?}", link, e),
         }
+    }
+
+    if use_clipboard {
+        cli_clipboard::set_contents(shortened_links.join("\n")).unwrap();
     }
 }
